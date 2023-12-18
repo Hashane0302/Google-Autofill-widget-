4e01d6c45a5a1766f0631dc84be6025853464394
Hashane <101983689+Hashane0302@users.noreply.github.com>
2023-12-18 19:01:22 +0530

    code updated with both billing and shiiping address autofilling

--- form.js
+++ form.js
@@ -1,121 +1,24 @@
-"use strict";
-
-const CONFIGURATION = {
-  ctaTitle: "Checkout",
-  mapOptions: {
-    center: { lat: 37.4221, lng: -122.0841 },
-    fullscreenControl: true,
-    mapTypeControl: false,
-    streetViewControl: true,
-    zoom: 18,
-    zoomControl: true,
-    maxZoom: 22,
-    mapId: "",
-  },
-  mapsApiKey: "AIzaSyAibYGrpQNEAgHN6fLC2v-iLJoaNuQxDSM",
-};
-
-const SHORT_NAME_ADDRESS_COMPONENT_TYPES = new Set([
-  "street_number",
-  "administrative_area_level_1",
-  "postal_code",
-]);
-
-const ADDRESS_COMPONENT_TYPES_IN_FORM = [
-  "location",
-  "locality",
-  "administrative_area_level_1",
-  "postal_code",
-  "country",
-];
-
-function getFormInputElement(componentType) {
-  return document.getElementById(`${componentType}-input`);
-}
-
-function fillInAddress(place) {
-  function getComponentName(componentType) {
-    for (const component of place.address_components || []) {
-      if (component.types[0] === componentType) {
-        return SHORT_NAME_ADDRESS_COMPONENT_TYPES.has(componentType)
-          ? component.short_name
-          : component.long_name;
-      }
-    }
-    return "";
-  }
-
-  function getComponentText(componentType) {
-    return componentType === "location"
-      ? `${getComponentName("street_number")} ${getComponentName("route")}`
-      : getComponentName(componentType);
-  }
-
-  for (const componentType of ADDRESS_COMPONENT_TYPES_IN_FORM) {
-    getFormInputElement(componentType).value = getComponentText(componentType);
-  }
-}
-
-function renderAddress(place, map, marker) {
-  if (place.geometry && place.geometry.location) {
-    map.setCenter(place.geometry.location);
-    marker.position = place.geometry.location;
-  } else {
-    marker.position = null;
-  }
-}
-
-async function initMap() {
-  const { Map } = google.maps;
-  const { AdvancedMarkerElement } = google.maps.marker;
-  const { Autocomplete } = google.maps.places;
-
-  const mapOptions = CONFIGURATION.mapOptions;
-  mapOptions.mapId = mapOptions.mapId || "DEMO_MAP_ID";
-  mapOptions.center = mapOptions.center || { lat: 37.4221, lng: -122.0841 };
-
-  const map = new Map(document.getElementById("gmp-map"), mapOptions);
-  const marker = new AdvancedMarkerElement({ map });
-  const autocomplete = new Autocomplete(getFormInputElement("location"), {
-    fields: ["address_components", "geometry", "name"],
-    types: ["address"],
-  });
-
-  autocomplete.addListener("place_changed", () => {
-    const place = autocomplete.getPlace();
-    if (!place.geometry) {
-      // User entered the name of a Place that was not suggested and
-      // pressed the Enter key, or the Place Details request failed.
-      window.alert(`No details available for input: '${place.name}'`);
-      return;
-    }
-    renderAddress(place, map, marker);
-    fillInAddress(place);
-  });
-}
-
 function copyShippingAddress() {
-  if (document.getElementById("myCheckbox").checked) {
-    document.querySelector(".billingAddress #location-input").value =
-      document.querySelector(".livingAddress #location-input").value;
-    document.querySelector(".billingAddress #locality-input").value =
-      document.querySelector(".livingAddress #locality-input").value;
-    document.querySelector(
-      ".billingAddress #administrative_area_level_1-input"
-    ).value = document.querySelector(
-      ".livingAddress #administrative_area_level_1-input"
-    ).value;
-    document.querySelector(".billingAddress #postal_code-input").value =
-      document.querySelector(".livingAddress #postal_code-input").value;
-    document.querySelector(".billingAddress #country-input").value =
-      document.querySelector(".livingAddress #country-input").value;
+  // Get the values from billing address fields
+  var billingStreet = document.getElementById('billing-street').value;
+  var billingCity = document.getElementById('billing-city').value;
+  var billingState = document.getElementById('billing-state').value;
+  var billingZip = document.getElementById('billing-zip').value;
+  var billingCountry = document.getElementById('billing-country').value;
+
+  // Set the values to shipping address fields if checkbox is checked
+  if (document.getElementById('myCheckbox').checked) {
+      document.getElementById('shipping-street').value = billingStreet;
+      document.getElementById('shipping-city').value = billingCity;
+      document.getElementById('shipping-state').value = billingState;
+      document.getElementById('shipping-zip').value = billingZip;
+      document.getElementById('shipping-country').value = billingCountry;
   } else {
-    document.querySelector(".billingAddress #location-input").value = "";
-    document.querySelector(".billingAddress #locality-input").value = "";
-    document.querySelector(
-      ".billingAddress #administrative_area_level_1-input"
-    ).value = "";
-    document.querySelector(".billingAddress #postal_code-input").value = "";
-    document.querySelector(".billingAddress #country-input").value = "";
+      // Clear shipping address fields if checkbox is unchecked
+      document.getElementById('shipping-street').value = '';
+      document.getElementById('shipping-city').value = '';
+      document.getElementById('shipping-state').value = '';
+      document.getElementById('shipping-zip').value = '';
+      document.getElementById('shipping-country').value = '';
   }
 }
